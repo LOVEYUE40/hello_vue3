0dc8b1aeec29679e964cd721b488bdf7b7959a63
LOVEYUE40 <50401223+LOVEYUE40@users.noreply.github.com>
2026-02-28 00:08:59 +0800

    Initial commit

---
+++ src/components/Person.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Person',
+}
+
+</script>
+
+<!-- 下面的写法是setup语法糖 -->
+<script setup lang="ts">
+console.log(this) // undefined
+
+// 数据（注意：此时的name、age、tel都不是响应式数据）
+let name = '张三'
+let age = 18
+let tel = '13800000000'
+
+// 方法
+function changeName() {
+    name = '李四' //注意：此时这么修改name页面是不变化的
+}
+function changeAge() {
+    age += 1 //注意：此时这么修改age页面是不变化的
+}
+function showTel() {
+    alert(tel)
+}
+</script>
+
+<style scoped>
+.person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
